899dfa5bf71fb620e62fe6f34c9b056c49589bfd
king-millez <Millez.Dev@gmail.com>
2020-10-30 10:39:48 +1100

    Start stream status check for tas_lower

--- legistream_backend/tas.py
+++ legistream_backend/tas.py
@@ -1,3 +1,5 @@
+import subprocess
+
 from requests import get
 
 base_stream_url = 'https://5ea8aa5cf299b.streamlock.net/'
@@ -7,3 +9,10 @@ class Stream(object):
         self.lower_stream_url = base_stream_url + 'HA/house_360p/playlist.m3u8'
         self.upper_stream_url = base_stream_url + 'LC/legco_360p/playlist.m3u8'
         self.stream_urls = {'lower': self.lower_stream_url, 'upper': self.upper_stream_url}
+
+    @property
+    def lower_is_live(self):
+        self.__dl_video(self.lower_stream_url)
+        
+    def __dl_video(self, input_url):
+        pass
