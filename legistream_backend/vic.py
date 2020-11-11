416f4a79948faaf2b20bf6fc991e1f0339d6a789
king-millez <Millez.Dev@gmail.com>
2020-11-11 11:26:22 +1100

    [vic] optimise object instances

--- legistream_backend/vic.py
+++ legistream_backend/vic.py
@@ -6,16 +6,22 @@ streams = ['https://pov_broadcast-i.akamaihd.net/hls/live/250374/pov-desk-la/mas
            'https://pov_broadcast-i.akamaihd.net/hls/live/250375/pov-desk-comm/master_900.m3u8']
 
 class Stream(object):
-    def __init__(self):
-        self.__return_data()
+    @property
+    def lower_stream_url(self):
+        return(streams[0])
+
+    @property
+    def upper_stream_url(self):
+        return(streams[1])
     
-    def __return_data(self):
-        self.lower_stream_url = streams[0]
-        self.upper_stream_url = streams[1]
-        self.committee_stream_url = streams[2]
-        self.stream_urls = {'lower': self.lower_stream_url, 'upper': self.upper_stream_url, 'committee': self.committee_stream_url}
-        
-    
+    @property
+    def committee_stream_url(self):
+        return(streams[2])
+
+    @property
+    def stream_urls(self):
+        return({'lower': self.lower_stream_url, 'upper': self.upper_stream_url, 'committee': self.committee_stream_url})
+
     @property
     def lower_is_live(self):
         if(get(self.lower_stream_url[:-15] + 'master_400.m3u8').status_code == 404):
