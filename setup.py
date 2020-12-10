98957e9ccf07f1fc71c11e8a7e736641cc0107e2
king-millez <Millez.Dev@gmail.com>
2020-12-10 14:03:43 +1100

    [pypi] change version to 1.0.2

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (here / 'README.md').read_text(encoding='utf-8')
 
 setup(
     name='legistream-backend',
-    version='1.0.0',
+    version='1.0.2',
     description='Get live stream metadata from the various Australian parliaments.',
     long_description=long_description,
     long_description_content_type='text/markdown',
