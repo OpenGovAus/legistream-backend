a0430f6f4529a72f81ec3d29a512a95bbb4fdd38
king-millez <Millez.Dev@gmail.com>
2021-02-13 14:17:12 +1100

    [act] better status & URL checkers

--- legistream_backend/act.py
+++ legistream_backend/act.py
@@ -1,20 +1,22 @@
-import m3u8
+import m3u8, json
 from requests import get
 
-api_url = 'http://video.dpa.act.gov.au/live/amlst:aod-recording/playlist.m3u8?DVR'
+api_url = 'http://aod.parliament.act.gov.au/api/video'
 
 class Stream(object):
-    def __init__(self):
-        self.__read_playlist(api_url)
-    
-    def __read_playlist(self, input_url):
-        playlist_data = m3u8.loads(get(input_url).text)
-        self.lower_stream_url = 'http://video.dpa.act.gov.au/live/amlst:aod-recording/' + playlist_data.data['playlists'][2]['uri']
-        self.stream_urls = {'lower': self.lower_stream_url}
+    __api_call = json.loads(get(api_url).text)['live']
 
     @property
+    def lower_stream_url(self):
+        return self.__api_call['url'] + self.__api_call['extension']
+    
+    @property
     def lower_is_live(self):
-        if(get(self.lower_stream_url).status_code == 404):
-            return(False)
+        if(m3u8.parse(get(self.lower_stream_url).text)['playlists']):
+            return True
         else:
-            return(True)
+            return False
+    
+    @property
+    def stream_urls(self):
+        return {'lower': self.lower_stream_url}
