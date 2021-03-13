68f1da26d4303dfd1a334626b34d13fee4989576
king-millez <Millez.Dev@gmail.com>
2021-03-13 17:13:58 +1100

    [tas] optimisations

--- legistream_backend/common.py
+++ legistream_backend/common.py
@@ -3,8 +3,8 @@ import sys
 operating_system = sys.platform
 
 if(operating_system != 'win32'):
-    root_dir = '/tmp/'
+    root_dir = '/tmp/.legistream/'
     ffmpeg_bin = 'ffmpeg'
 else:
-    root_dir = 'C:/temp/'
+    root_dir = 'C:/temp/.legistream/'
     ffmpeg_bin = 'ffmpeg.exe'
--- legistream_backend/tas.py
+++ legistream_backend/tas.py
@@ -1,18 +1,11 @@
-import os
-import m3u8
-import subprocess
-import imagehash
 from PIL import Image
-from datetime import datetime
 from requests import get
-
+from bs4 import BeautifulSoup
+from datetime import datetime
+import os, m3u8, subprocess, imagehash, shutil
 from . import common
 
 filepath = os.path.dirname(os.path.realpath(__file__))
-
-base_stream_url = 'https://5ea8aa5cf299b.streamlock.net/'
-lower_base_url = 'HA/house_360p/'
-upper_base_url = 'LC/legco_360p/'
 
 class Stream(object):
     lower_img_hash = imagehash.average_hash(Image.open(filepath + '/tas_img/ha_adjourned.png'))
@@ -20,11 +13,11 @@ class Stream(object):
 
     @property
     def lower_stream_url(self):
-        return(base_stream_url + lower_base_url + 'playlist.m3u8')
+        return BeautifulSoup(get('https://www.parliament.tas.gov.au/TBS/havideo.html').text, 'lxml').find('source')['src']
 
     @property
     def upper_stream_url(self):
-        return(base_stream_url + upper_base_url + 'playlist.m3u8')
+        return BeautifulSoup(get('https://www.parliament.tas.gov.au/TBS/LCvideo.html').text, 'lxml').find('source')['src']
 
     @property
     def stream_urls(self):
@@ -34,7 +27,7 @@ class Stream(object):
     @property
     def lower_is_live(self):
         try:
-            if(self.lower_img_hash - self.__get_vid_hash(self.lower_stream_url, lower_base_url) < 5):
+            if(self.lower_img_hash - self.__get_vid_hash(self.lower_stream_url) < 5):
                 return(False)
             else:
                 return(True)
@@ -43,32 +36,40 @@ class Stream(object):
         
     @property
     def upper_is_live(self):
-        try:
-            if(self.upper_img_hash - self.__get_vid_hash(self.upper_stream_url, upper_base_url) < 5):
-                return(False)
-            else:
-                return(True)
-        except:
-            return False
+        if(self.upper_img_hash - self.__get_vid_hash(self.upper_stream_url) < 5):
+            return(False)
+        else:
+            return(True)
 
-    def __get_vid_hash(self, input_url, base_url):
+    def __cleanup(self, directory): # Clear Legistream's temp directory
+        try:
+            if(os.path.exists(directory)):
+                shutil.rmtree(directory)
+        except Exception as e:
+            print(e)
+
+    def __get_vid_hash(self, input_url):
         try:
             try:
                 playlist_data = m3u8.loads(get(input_url).text)
             except:
                 return None
-            seg_uri = m3u8.loads(get(base_stream_url + base_url + playlist_data.data['playlists'][0]['uri']).text).data['segments'][-1]['uri']
-            seg_ts = get(base_stream_url + base_url + seg_uri)
+            seg_uri = m3u8.loads(get(input_url.replace('playlist.m3u8', '') + playlist_data.data['playlists'][0]['uri']).text).data['segments'][-1]['uri']
+            seg_ts = get(input_url.replace('playlist.m3u8', '') + seg_uri)
             current_time = str(datetime.now()).replace(':', '-').replace('.', '-').replace(' ', '_')
+            if(not os.path.exists(common.root_dir)):
+                os.mkdir(common.root_dir)
+
             seg_output_file = common.root_dir + current_time + '_tas_seg.ts'
             with open(seg_output_file, 'wb') as file:
                 file.write(seg_ts.content)
             
-            img_out = common.root_dir + current_time + '_tas_seg_out.png'
+            img_out = common.root_dir + current_time + '_tas_seg_out.jpg'
             
             command = ['-ss', '00:00:00', '-i', seg_output_file, '-frames:v', '1', img_out]
             subprocess.run([common.ffmpeg_bin] + command, capture_output=True)
-            
-            return(imagehash.average_hash((Image.open(img_out))))
+            _hash = imagehash.average_hash((Image.open(img_out)))
+            self.__cleanup(common.root_dir)
+            return _hash
         except:
             raise Exception('Invalid input URL: ' + input_url)
