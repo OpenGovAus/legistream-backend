13ce02daa5627d135e18448ce52752fe3261fcfd
king-millez <millez.dev@gmail.com>
2021-06-28 22:28:11 +1000

    [tas] fix imagehash difference being too low

--- legistream_backend/tas.py
+++ legistream_backend/tas.py
@@ -1,9 +1,17 @@
+import os
+import m3u8
+import subprocess
+import imagehash
+import shutil
+
 from PIL import Image
 from requests import get
 from bs4 import BeautifulSoup
 from datetime import datetime
-import os, m3u8, subprocess, imagehash, shutil
-from . import common
+
+from legistream_backend import common
+from legistream_backend.util.fileutils import check_tempdir
+
 
 filepath = os.path.dirname(os.path.realpath(__file__))
 
@@ -27,16 +35,16 @@ class Stream(object):
     @property
     def lower_is_live(self):
         try:
-            if(abs(self.lower_img_hash - self.__get_vid_hash(self.lower_stream_url)) < 5):
+            if(abs(self.lower_img_hash - self.__get_vid_hash(self.lower_stream_url)) < 16384):
                 return(False)
             else:
                 return(True)
-        except:
+        except Exception:
             return False
         
     @property
     def upper_is_live(self):
-        if(abs(self.upper_img_hash - self.__get_vid_hash(self.upper_stream_url)) < 5):
+        if(abs(self.upper_img_hash - self.__get_vid_hash(self.upper_stream_url)) < 16384):
             return(False)
         else:
             return(True)
@@ -49,27 +57,24 @@ class Stream(object):
             print(e)
 
     def __get_vid_hash(self, input_url):
+        check_tempdir(common.root_dir)
         try:
-            try:
-                playlist_data = m3u8.loads(get(input_url).text)
-            except:
-                return None
-            seg_uri = m3u8.loads(get(input_url.replace('playlist.m3u8', '') + playlist_data.data['playlists'][0]['uri']).text).data['segments'][-1]['uri']
-            seg_ts = get(input_url.replace('playlist.m3u8', '') + seg_uri)
-            current_time = str(datetime.now()).replace(':', '-').replace('.', '-').replace(' ', '_')
-            if(not os.path.exists(common.root_dir)):
-                os.mkdir(common.root_dir)
+            playlist_data = m3u8.loads(get(input_url).text)
+        except Exception:
+            return None
+        seg_uri = m3u8.loads(get(input_url.replace('playlist.m3u8', '') + playlist_data.data['playlists'][0]['uri']).text).data['segments'][-1]['uri']
+        seg_ts = get(input_url.replace('playlist.m3u8', '') + seg_uri)
+        current_time = str(datetime.now()).replace(':', '-').replace('.', '-').replace(' ', '_')
 
-            seg_output_file = common.root_dir + current_time + '_tas_seg.ts'
-            with open(seg_output_file, 'wb') as file:
-                file.write(seg_ts.content)
-            
-            img_out = common.root_dir + current_time + '_tas_seg_out.jpg'
-            
-            command = ['-ss', '00:00:00', '-i', seg_output_file, '-frames:v', '1', img_out]
-            subprocess.run([common.ffmpeg_bin] + command, capture_output=True)
-            _hash = int(str(imagehash.average_hash((Image.open(img_out)))), 16)
-            self.__cleanup(common.root_dir)
-            return _hash
-        except:
-            raise Exception('Invalid input URL: ' + input_url)
+        seg_output_file = common.root_dir + current_time + '_tas_seg.ts'
+        with open(seg_output_file, 'wb') as file:
+            file.write(seg_ts.content)
+        
+        img_out = common.root_dir + current_time + '_tas_seg_out.jpg'
+        
+        command = ['-ss', '00:00:00', '-i', seg_output_file, '-frames:v', '1', img_out]
+        subprocess.run([common.ffmpeg_bin] + command, capture_output=True)
+        _hash = int(str(imagehash.average_hash((Image.open(img_out)))), 16)
+        print('bruh', _hash)
+        self.__cleanup(common.root_dir)
+        return _hash
---
+++ legistream_backend/util/fileutils.py
@@ -0,0 +1,9 @@
+import os
+import os.path as path
+
+def check_tempdir(dirpath):
+    if not path.exists(path.realpath(dirpath)):
+        if not path.exists(path.dirname(path.realpath(dirpath))):
+            os.mkdir(path.dirname(path.realpath(dirpath)))
+        else:
+            os.mkdir(path.realpath(dirpath))
