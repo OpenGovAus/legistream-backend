8371c846852f55176b027d8854dc89d4e988a584
king-millez <Millez.Dev@gmail.com>
2021-08-13 14:29:30 +1000

    [refactoring] qld

--- legistream_backend/site/qld.py
+++ legistream_backend/site/qld.py
@@ -1,54 +1,56 @@
-import m3u8, json, re
-from requests import get
-from bs4 import BeautifulSoup
+import re
+from typing import List
 
-CALENDAR_URL = 'http://tv.parliament.qld.gov.au/TV/PartialCalendar'
 
-class Stream(object):
-    @property
-    def is_live(self):
-        try:
-            _m3u8 = get(self.stream_url)
-            if(_m3u8.status_code != 200):
-                return False
+from legistream_backend import StreamExtractor
+from legistream_backend.util.models import StreamModel
 
-            self.m3u8_data = m3u8.parse(_m3u8).text
-            if(self.m3u8_data['playlists']):
-                return True
-            else:
-                return False
-        except:
-            return False
+
+class QLDStreamExtractor(StreamExtractor):
+
+    BASE = 'https://tv.parliament.qld.gov.au'
 
     @property
-    def stream_url(self):
-        _js = BeautifulSoup(get('http://tv.parliament.qld.gov.au/TV/PartialGetHomeContent').text, 'lxml').find('script').string.split('\n')
-        
-        for index,line in enumerate(_js):
-            if(line.strip() == 'var videoSources = ['):
-                try:
-                    return json.loads(_js[index + 1].replace('];', '').replace(',', '').strip().replace("'", '"'))['file']
-                except:
-                    return ''
+    def extractor_name(self):
+        return 'QLD'
 
     @property
-    def stream_title(self):
-        if(self.is_live):
-            soup = BeautifulSoup(get(CALENDAR_URL).text, 'lxml')
-            try:
-                _title = soup.find_all('h3')[-1].text.strip()
-                if(_title == 'House Sitting Date'):
-                    return 'Legislative Assembly'
-                else:
-                    return _title
-            except:
-                return ''
+    def streams(self) -> List[StreamModel]:
+        return self._get_streams()
+
+    def _get_streams(self) -> List[StreamModel]:
+        homepage = self._download_html(self.BASE)
+
+        if re.search(r'IsBroadcasting=True', str(homepage)):
+            is_live = True
+            watch_page = self._download_html(
+                f'{self.BASE}/TV/PartialGetHomeContent?'
+                f'IsBroadcasting=True&Reference='
+            )
+            stream_url = re.search(
+                r"{ 'file': '(https?:\/\/[aA-zZ0-9\-\.\/]+"
+                r"@[0-9/aA-zZ]+\.m3u8)",
+                str(watch_page)).group(1)
+
+            stream_title = self._parse_cal(
+                f'{self.BASE}/TV/PartialCalendar')
+            print(stream_title)
         else:
-            return ''
+            is_live = False
+            stream_url = ''
+            stream_title = ''
 
-    @property
-    def stream_urls(self):
-        if(self.is_live):
-            return {self.stream_title: self.stream_url}
-        else:
-            return {}
+        model = StreamModel(
+            url=stream_url,
+            is_live=is_live,
+            title=stream_title
+        )
+
+        return [model]
+
+    def _parse_cal(self, url: str) -> str:
+        page = self._download_html(url)
+        most_recent = page.find_all(
+            'div', {'class': 'row latest-posts-classic pad5B'})[-1] \
+            .find('h3').text
+        return most_recent
