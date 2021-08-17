847fd64f5b2e541af4213288c985c810018b459c
king-millez <Millez.Dev@gmail.com>
2021-08-17 16:47:06 +1000

    [wa] new backend for WA

--- legistream_backend/site/wa.py
+++ legistream_backend/site/wa.py
@@ -1,58 +1,88 @@
-from typing import Text
-import m3u8
-import urllib3
-from bs4 import BeautifulSoup
-from requests import get
+from typing import List
+from legistream_backend import StreamExtractor
+from legistream_backend.util.models import StreamModel
 
-urllib3.disable_warnings()
 
-lower_master = 'https://streamer1.parliament.wa.gov.au/live/smil:AssemblyLinqStream.smil/playlist.m3u8'
-upper_master = 'https://streamer2.parliament.wa.gov.au/live/smil:CouncilLinqStream.smil/playlist.m3u8'
-stream_page_url = 'https://parliament.wa.gov.au/WebCMS/webcms.nsf/watch-live'
-
-class Stream(object):
-    @property
-    def lower_is_live(self):
-        try:
-            box = BeautifulSoup(get(stream_page_url, verify=False).text, 'lxml').find('div', {'class': 'watchbox la'})
-            heading = box.find('h6', string='Nothing Scheduled')
-            if(heading):
-                return False
-            else:
-                return True
-        except:
-            return False
+class WAStreamExtractor(StreamExtractor):
 
     @property
-    def upper_is_live(self):
-        try:
-            box = BeautifulSoup(get(stream_page_url, verify=False).text, 'lxml').find('div', {'class': 'watchbox lc'})
-            heading = box.find('h6', string='Nothing Scheduled')
-            if(heading):
-                return False
-            else:
-                return True
-        except:
-            return False
+    def extractor_name(self):
+        return 'WA'
 
     @property
-    def lower_stream_url(self):
-        if(self.lower_is_live):
-            return('https://streamer1.parliament.wa.gov.au/live/smil:AssemblyLinqStream.smil/' + self.__get_playlist(lower_master))
-        else:
-            return ''
+    def streams(self) -> List[StreamModel]:
+        return self._get_streams()
 
-    @property
-    def upper_stream_url(self):
-        if(self.upper_is_live):
-            return('https://streamer2.parliament.wa.gov.au/live/smil:CouncilLinqStream.smil/' + self.__get_playlist(upper_master))
-        else:
-            return ''
+    def _get_streams(self) -> List[StreamModel]:
+        BRIGHTCOVE_API = 'https://edge.api.brightcove.com/playback/v1/acco' \
+            'unts/6193427228001/videos/'
+        ASSEMBLY = 'assembly'
+        COUNCIL = 'council'
+        COMMITTEE = 'committee'
+        houses = {
+            ASSEMBLY: 'Legislative Assembly',
+            COUNCIL: 'Legislative Council'
+        }
+        thumbs = {
+            ASSEMBLY: 'wa_la.webp',
+            COUNCIL: 'wa_lc.webp',
+            COMMITTEE: 'wa_com.webp'
+        }
+        streams_list = []
+        for stream_page in [ASSEMBLY, COUNCIL]:
+            page_url = f'https://{stream_page}linq.parliament.wa.gov.au/'
+            dummy = StreamModel(
+                    url='',
+                    is_live=False,
+                    title=houses[stream_page],
+                    thumb=thumbs[stream_page]
+                )
+            landing = self._download_html(
+                f'https://{stream_page}linq.parliament.wa.gov.au/')
+            try:
+                viewstate = {
+                    '__EVENTVALIDATION': landing.find(
+                        'input', {'name': '__EVENTVALIDATION'})['value'],
 
-    @property
-    def stream_urls(self):
-        return({'lower': self.lower_stream_url, 'upper': self.upper_stream_url})
+                    '__EVENTTARGET': 'ctl00$PlaceHolderMain$VideoButton',
 
-    def __get_playlist(self, input_url):
-        dat = m3u8.parse(get(input_url, verify=False).text)
-        return(dat['playlists'][0]['uri'])
+                    '__VIEWSTATEGENERATOR': landing.find(
+                        'input', {'name': '__VIEWSTATEGENERATOR'})['value'],
+                    '__EVENTARGUMENT': '',
+
+                    '__VIEWSTATE': landing.find(
+                        'input', {'name': '__VIEWSTATE'})['value']
+                }
+                redir_url = self._download_page(
+                    page_url,
+                    method='POST',
+                    postdata=viewstate
+                ).url
+                if redir_url == page_url:
+                    streams_list.append(dummy)
+                else:
+                    watch_page = self._download_html(redir_url)
+                    brightcove_id = watch_page.find(
+                        'video-js')['data-video-id']
+                    stream_api = self._download_json(
+                        BRIGHTCOVE_API + brightcove_id, headers={
+                            'Accept': 'application/json;pk=BCpkADawqM2ekcVYLH'
+                                      'OvfmPvdxxol_QHum36qVcEDQmI8Hp5YIFuOoyZ'
+                                      'jY8gmRg8B6TJEvX-KueK6KTlLjxw9xKJ4V-9UD'
+                                      '1LAuRtCZ5tBdigaZxoq39BEqe2MWzkMOdVs1wr'
+                                      'NANvIqKM8-H6'
+                        })
+                    stream_url = stream_api['sources'][0]['src']
+                    streams_list.append(StreamModel(
+                        url=stream_url,
+                        is_live=True,
+                        title=houses[stream_page],
+                        thumb=thumbs[stream_page]
+                    ))
+
+            except Exception as e:
+                print(f'Could not find VIEWSTATE info for '
+                      f'{stream_page}\n\n{str(e)}')
+                streams_list.append(dummy)
+
+        return streams_list
